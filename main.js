f376677ad169a580fc632c64a9cafea75bdad61b
jaeder <zxb360@gmail.com>
2023-12-03 10:05:41 -0300

    repositorio

---
+++ main.js
@@ -0,0 +1,21 @@
+import { nanoid } from 'nanoid';
+// import copy from 'clipboard-copy';
+
+console.log(nanoid());
+
+// const passowrd = document.querySelector('button');
+// const verivificandoNanoId = nanoid();
+// console.log(verivificandoNanoId);
+// const displayPass = document.querySelector('h3');
+
+
+// passowrd.addEventListener('click', () => {
+//     const randownPass = nanoid();
+//     displayPass.innerHTML = randownPass;
+//     console.log("button");
+// });
+
+// displayPass.addEventListener('click', () => {
+//     copy(event.target.innerHTML);
+//     alert('COPIADO');
+// });
